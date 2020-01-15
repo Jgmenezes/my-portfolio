88420be87b5ab0813760e3c754edebe8e2e809f4
jgmenezes <jgmenezes@live.com>
2020-01-15 08:01:44 -0300

    (change): change navigation item text font size

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,7 +1,7 @@
 const theme = {
     NavigationItem_Text: `
         color: #ffffff;
-        font-size: calc(0.6rem + 0.5vw);
+        font-size: calc(1rem + 0.5vw);
         font-family: "Helvetica Neue", sans-serif;
     `
 }
